73327179479379093897820dca33ea0b7ce34b92
Mani Kumar <purammanikumar@gmail.com>
2021-01-19 01:45:26 -0500

    one more change

--- java.js
+++ java.js
@@ -2,3 +2,9 @@ console.log("This is my java console")
 let a = parseInt('2.43')
 let b = parseFloat('2.43')
 console.log(a, b);
+let foods = ['pizza', 'tacos', 'burgers']
+foods.push('chocos', 'bikis');
+foods.push('kitkat', 'bourbon');
+// foods.pull('chocos', 'tacos');
+let items = foods.toString()
+console.log(items);
